e498fb15ec48fd2308b47b57551044e874304e6a
gjj <gaojj256@gmail.com>
2023-05-18 13:42:31 +0800

    finish vmprint

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -11,16 +11,17 @@
 
 void freerange(void *pa_start, void *pa_end);
 
-extern char end[]; // first address after kernel.
+extern char end[]; // first address after kernel. 指向内核映像的末尾地址
                    // defined by kernel.ld.
 
+// 空闲链表
 struct run {
   struct run *next;
 };
 
 struct {
-  struct spinlock lock;
-  struct run *freelist;
+  struct spinlock lock;  // 自旋锁
+  struct run *freelist;  // 指向可用分配空间的头节点 
 } kmem;
 
 void
@@ -39,10 +40,13 @@ freerange(void *pa_start, void *pa_end)
     kfree(p);
 }
 
-// Free the page of physical memory pointed at by pa,
-// which normally should have been returned by a
+// Free the page of physical memory pointed at by pa,  释放pa指向的物理内容
+// which normally should have been returned by a   
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
+// 函数用于将一个物理内存页面释放，它通常应该是通过调用 kalloc() 函数分配的。
+// 该函数先检查要释放的内存页面地址是否合法，然后使用 memset() 函数将页面填充为垃圾值，
+// 以便检测未释放的引用。最后，将该页面添加到空闲链表的开头。
 void
 kfree(void *pa)
 {
@@ -62,9 +66,9 @@ kfree(void *pa)
   release(&kmem.lock);
 }
 
-// Allocate one 4096-byte page of physical memory.
-// Returns a pointer that the kernel can use.
-// Returns 0 if the memory cannot be allocated.
+// Allocate one 4096-byte page of physical memory. 分配一个4096B的物理页面
+// Returns a pointer that the kernel can use.  返回一个内核可用的指针
+// Returns 0 if the memory cannot be allocated.  如果无法分配的话返回0
 void *
 kalloc(void)
 {
@@ -81,6 +85,7 @@ kalloc(void)
   return (void*)r;
 }
 
+// sysinfo实验内容
 uint64 
 acq_mem(void)
 {
--- kernel/vm.c
+++ kernel/vm.c
@@ -437,3 +437,31 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void _vmprint(pagetable_t pagetable, int level)
+{
+    for(int i = 0; i < 512; i++){
+        pte_t pte = pagetable[i];
+        if(pte & PTE_V){
+            for(int j = 0; j < level; j ++){
+                if(j) printf(" ");
+                printf("..");
+            }
+            uint64 child = PTE2PA(pte);
+            // 0: pte 0x0000000021fda801 pa 0x0000000087f6a000
+            printf("%d: pte %p pa %p \n", i, pte, child);
+            if((pte & (PTE_R|PTE_W|PTE_X)) == 0){
+                // this PTE points to a lower-level page table.
+                _vmprint((pagetable_t)child, level + 1);
+            } 
+        }
+    }
+}
+
+void vmprint(pagetable_t pagetable)
+{
+    printf("page table %p\n", pagetable);
+    _vmprint(pagetable, 1);
+    // there are 2^9 = 512 PTEs in a page table.
+    
+}
